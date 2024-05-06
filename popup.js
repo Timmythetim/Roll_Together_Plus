6b06cd31459706e0cd02f68926caced4fb385c31
Timmythetim <tim.reidy@wsu.edu>
2024-05-06 11:05:12 -0700

    restructured directory to be more fit with developing extensions

--- popup.js
+++
@@ -1,8 +0,0 @@
-const tab = await chrome.tabs.query({
-    currentWindow : true, active : true
-  });
-const button = document.querySelector("button");
-button.addEventListener("click", async () => {
-        chrome.runtime.sendMessage({timestamp: tab});
-})
-  
--- scripts/background.js
+++
@@ -1,5 +0,0 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    //This should eventually get information about creating a room, then send it to the python backend
-    console.log(request.timestamp)
-
-});
